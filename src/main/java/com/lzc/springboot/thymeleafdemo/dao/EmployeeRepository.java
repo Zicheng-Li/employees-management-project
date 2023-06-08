424a182a9a24684bd3e8a6dd601264aed3e46f4b
Zicheng Li <103531886+Zicheng-Li@users.noreply.github.com>
2023-06-08 16:32:51 -0400

    Update EmployeeRepository.java

--- src/main/java/com/lzc/springboot/thymeleafdemo/dao/EmployeeRepository.java
+++ src/main/java/com/lzc/springboot/thymeleafdemo/dao/EmployeeRepository.java
@@ -7,8 +7,6 @@ import java.util.List;
 
 public interface EmployeeRepository extends JpaRepository<Employee, Integer> {
 
-	// that's it ... no need to write any code LOL!
-
     // add a method to sort by last name
     public List<Employee> findAllByOrderByLastNameAsc();
 
